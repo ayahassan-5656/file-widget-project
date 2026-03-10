5f068c00864e254524a3b47b72c4810f579d78c4
ayahassan-5656 <aya.hassan@sdggroup.com>
2026-03-10 12:36:29 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,7 @@ const fileRoutes = require("./routes/files");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: [
-    "https://ayahassan-5656.github.io"
-  ]
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
