60a9b2f8e5c9623680ed400367e8c85ac5d23cc6
ayahassan-5656 <aya.hassan@sdggroup.com>
2026-03-10 12:08:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-class SACFileManager extends HTMLElement {
-  constructor() {
-    super();
-    const root = this.attachShadow({ mode: "open" });
-    root.innerHTML = `
-      <div style="padding:16px;font-family:Arial,sans-serif;">
-        <h3 style="margin:0 0 8px 0;">File Actions Manager</h3>
-        <div>Widget loaded successfully.</div>
-      </div>
-    `;
-  }
-
-  setUniqueFileKey(key) {
-    this._uniqueFileKey = key || "";
-  }
-
-  getUniqueFileKey() {
-    return this._uniqueFileKey || "";
-  }
-
-  setApiBaseUrl(url) {
-    this._apiBaseUrl = url || "";
-  }
-
-  getApiBaseUrl() {
-    return this._apiBaseUrl || "";
-  }
-
-  refreshFiles() {
-    // no-op for now
-  }
+{
+  "id": "file_actions",
+  "version": "1.0.0",
+  "name": "File Actions Manager",
+  "newInstancePrefix": "FileActionsManager",
+  "description": "File upload manager",
+  "webcomponents": [
+    {
+      "kind": "main",
+      "tag": "file-actions-manager",
+      "url": "/main.js",
+      "integrity": "",
+      "ignoreIntegrity": true
+    }
+  ],
+  "properties": {
+    "apiBaseUrl": {
+      "type": "string",
+      "default": ""
+    }
+  },
+  "methods": {
+    "refreshFiles": {
+      "description": "Reload files"
+    }
+  },
+  "events": {}
 }
-
-customElements.define("file-actions-manager", SACFileManager);
